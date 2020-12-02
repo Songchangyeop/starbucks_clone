2443b13d9dcce2a1ddf052d79d19cea443a4ca47
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2020-12-03 04:11:26 +0900

    JS : Carousel Slider 구현

--- hide_slider.js
+++ hide_slider.js
@@ -6,46 +6,89 @@ const slideList = document.querySelector('.hide_slide-list');
 const slideBox = document.querySelector('.hide_slide-box');
 const nextBtn = document.querySelector('.right_swiper')
 const prevBtn = document.querySelector('.left_swiper')
+const slideItem = document.querySelectorAll('.hide_slide-item');
 
-const itemSize = 820;
+const itemSize = 840;
 
-slideList.style.width = `${4100}px`;
+
+slideList.style.width = `${itemSize * 7}px`;
 
 let currentNum = 1;
+slideList.style.transform = `translateX(-${itemSize * (currentNum)}px)`;
 
-let First = slideList.firstElementChild;
-let Last = slideList.lastElementChild;
-let cloneFirst = First.cloneNode(true);
-let cloneLast = Last.cloneNode(true);
-slideList.insertBefore(cloneLast, slideList.firstElementChild);
-slideList.appendChild(cloneFirst);
+let Second = slideList.childNodes[5];
+let cloneSecond_1 = Second.cloneNode(true);
+let cloneSecond_2 = Second.cloneNode(true);
+slideList.appendChild(cloneSecond_1);
+slideList.insertBefore(cloneSecond_2, slideList.firstElementChild);
+
+let curSlide = slideItem[currentNum];
+curSlide.classList.add('active');
 
 
 nextBtn.addEventListener('click', () => {
-    // if(currentNode.classList[1] !== 'active') {
-    //     return;
-    // }
-    console.log(`전 ${currentNum}`);
-    slideList.style.transform = `translateX(${-itemSize * currentNum}px)`;
-    currentNum++;
-    console.log(`현재 ${currentNum}`);
-
-    if(currentNum > 3) {
-        currentNum = 1;
-        console.log(`오버 ${currentNum}`);
+    if(curSlide.classList[1] !== 'active') {
+        return;
     }
+
+    if(currentNum >= 0){
+        slideList.style.transition = `${300}ms`
+        slideList.style.transform = `translateX(-${itemSize * (currentNum + 1)}px)`;
+        if(currentNum == 3) {
+            curSlide.classList.remove('active');
+            setTimeout(() => {
+                currentNum = 1;
+                slideList.style.transition = `${0}ms`
+                slideList.style.transform = `translateX(-${itemSize * (currentNum)}px)`;
+                curSlide = slideItem[currentNum];
+                curSlide.classList.add('active'); 
+            }, 300);
+        }
+    }
+
+    if(currentNum !== 3){
+        curSlide.classList.remove('active');
+        curSlide = slideItem[++currentNum];
+    setTimeout(() => {
+        curSlide.classList.add('active');            
+    }, 300);
+}
 });
 
 
+prevBtn.addEventListener('click', () => {
+    if(curSlide.classList[1] !== 'active') {
+        return;
+    }
 
+    if(currentNum >= 0){
+        slideList.style.transition = `${300}ms`
+        slideList.style.transform = `translateX(-${itemSize * (currentNum - 1)}px)`;
+    }
+
+    if(currentNum !== 0){
+        curSlide.classList.remove('active');
+        curSlide = slideItem[--currentNum];
+        if(currentNum == 0) {
+            curSlide.classList.remove('active');
+            setTimeout(() => {
+                currentNum = 3;
+                slideList.style.transition = `${0}ms`
+                slideList.style.transform = `translateX(-${itemSize * (currentNum)}px)`;
+                curSlide = slideItem[currentNum];
+            }, 300);
+        }
+    setTimeout(() => {
+        curSlide.classList.add('active');            
+    }, 300);
+}
+});
 
 hideBtn.addEventListener('click', () => {
     if(hideSlide.classList[1] == 'close'){
-    // hideSlide.style.visibility = `visible`;
     hideSlide.style.height = `${658}px`;
     hideSlide.classList.remove('close');
     } else {
-    // hideSlide.style.visibility = `hidden`;
     hideSlide.style.height = `${0}px`;
     hideSlide.classList.add('close');
     
