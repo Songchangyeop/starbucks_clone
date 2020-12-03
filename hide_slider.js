cf537e7019639f4200016f2c89a365a0444c83e0
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2020-12-04 03:20:18 +0900

    HTML + CSS + JS : Main Visual appear 구현

--- hide_slider.js
+++ hide_slider.js
@@ -6,15 +6,17 @@ const slideList = document.querySelector('.hide_slide-list');
 const slideBox = document.querySelector('.hide_slide-box');
 const nextBtn = document.querySelector('.right_swiper')
 const prevBtn = document.querySelector('.left_swiper')
+const swiperBox= document.querySelector('.slide_swiper-box');
 const slideItem = document.querySelectorAll('.hide_slide-item');
 
 const itemSize = 840;
 
-
-slideList.style.width = `${itemSize * 7}px`;
+function init() {
+    slideList.style.width = `${itemSize * 7}px`;
+    slideList.style.transform = `translateX(-${itemSize * (currentNum)}px)`;
+}
 
 let currentNum = 1;
-slideList.style.transform = `translateX(-${itemSize * (currentNum)}px)`;
 
 let Second = slideList.childNodes[5];
 let cloneSecond_1 = Second.cloneNode(true);
@@ -88,10 +90,14 @@ hideBtn.addEventListener('click', () => {
     if(hideSlide.classList[1] == 'close'){
     hideSlide.style.height = `${658}px`;
     hideSlide.classList.remove('close');
+    setTimeout(() => {
+        swiperBox.style.opacity = '1';
+    }, 500);
     } else {
     hideSlide.style.height = `${0}px`;
     hideSlide.classList.add('close');
-    
+    swiperBox.style.opacity = '0';
     }
 });
 
+init();
---
+++ main_appear.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const innerSet = document.querySelectorAll('.inner-set');
+const innerSlogan = document.querySelector('.main__inner-slogan');
+const btnSlogan  = document.querySelector('.btn_slogan');
+
+
+function delay(time, index, node) {
+    if(node == undefined){
+        setTimeout(() => {
+            innerSet[index].style.opacity = '1';
+        }, time);
+    }
+    
+    if(node == innerSlogan) {
+        setTimeout(() => {
+            node.style.opacity = '1';
+        }, time);
+    }
+
+    if(node == btnSlogan) {
+        setTimeout(() => {
+            node.style.opacity = '1';
+        }, time);
+    }
+};
+
+function appearContent() {
+    delay(500, 1, undefined);
+    delay(1000, 3, undefined);
+    delay(1500, 2, undefined);
+    delay(2000, 4, undefined);
+    delay(2500, 0, undefined);
+    delay(3000, 0, innerSlogan);
+    delay(3500, 0, btnSlogan);
+}
+
+appearContent();
+
